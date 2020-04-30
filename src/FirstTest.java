12a6c5c4b84ed85a6694b7545c78b4a582528ad2
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-04-30 16:33:52 +0300

    Ex2
    Создание метода

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -33,6 +37,31 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        System.out.println("First test run");
+        WebElement element_onboarding = driver.findElementByXPath("//*[contains(@text, 'SKIP')]");
+        element_onboarding.click();
+
+        WebElement element_to_init_search = waitForElementPresentByXpath(
+                "//android.widget.ImageView[@content-desc=\"Search Wikipedia\"]",
+                "Cannot find element_to_init_search");
+        element_to_init_search.click();
+
+        WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+                "//*[contains(@text, 'Search Wikipedia')]",
+                "Cannot find search input");
+        element_to_enter_search_line.sendKeys("Appium");
+
+
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message) {
+        return waitForElementPresentByXpath(xpath, error_message, 5);
     }
 }
