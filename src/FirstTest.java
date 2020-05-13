56edbd82f06d247b2b8a942b659f43cb657606c0
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-05-13 22:13:26 +0300

    Ex7

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -36,7 +37,10 @@ public class FirstTest {
 
     @After
     public void tearDown() {
-        driver.quit();
+        if (driver.getOrientation().equals(ScreenOrientation.LANDSCAPE)) {
+            driver.rotate(ScreenOrientation.PORTRAIT);
+        }
+            driver.quit();
     }
 
     @Test
@@ -468,6 +472,64 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void testChangeScreenOrientationOnSearchResults(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'SKIP')]"),
+                "Cannot find 'Skip' button",
+                1);
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find element_to_init_search",
+                5);
+
+        String search_line = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                search_line,
+                "Cannot find search input",
+                5);
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic searching by " + search_line,
+                15);
+
+        String title_before_location = waitForElementAndGetAttribute(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "text",
+                "Cannot find title of article",
+                15);
+
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+
+        String title_after_rotation = waitForElementAndGetAttribute(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "text",
+                "Cannot find title of article",
+                15);
+
+        Assert.assertEquals(
+                "Article title have been changed after screen rotation",
+                title_before_location,
+                title_after_rotation);
+
+        driver.rotate(ScreenOrientation.PORTRAIT);
+
+        String title_after_second_rotation = waitForElementAndGetAttribute(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "text",
+                "Cannot find title of article",
+                15);
+
+        Assert.assertEquals(
+                "Article title have been changed after second screen rotation",
+                title_before_location,
+                title_after_second_rotation);
+
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -578,5 +640,8 @@ public class FirstTest {
         }
     }
 
-
+    private String waitForElementAndGetAttribute(By by, String attribute, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        return element.getAttribute(attribute);
+    }
 }
