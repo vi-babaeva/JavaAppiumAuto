5c52d6ce5c7f6a3b7ad018b008b1fc12b246bf84
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-05-26 12:56:00 +0300

    app changed

--- apks/Wikipedia.app/Properties.js
+++
@@ -1,6 +0,0 @@
-const leadImage = pcs.c1.Page.getLeadImage();
-window.webkit.messageHandlers.{{messageHandlerName}}.postMessage({action: 'leadImage', data: {leadImage}});
-const tableOfContents = pcs.c1.Page.getTableOfContents();
-if (tableOfContents) {
-    window.webkit.messageHandlers.{{messageHandlerName}}.postMessage({action: 'tableOfContents', data: {tableOfContents}});
-}
