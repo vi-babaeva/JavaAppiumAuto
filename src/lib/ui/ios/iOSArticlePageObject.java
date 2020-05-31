5b80b35dc69b49366e78287caf7576bd28f24265
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-05-31 21:28:14 +0300

    Ex11_2

--- src/lib/ui/ios/iOSArticlePageObject.java
+++ src/lib/ui/ios/iOSArticlePageObject.java
@@ -9,7 +9,7 @@ public class iOSArticlePageObject extends ArticlePageObject {
         SUBTITLE = "id:Object-oriented programming language";
         FOOTER_ELEMENT = "id:View article in browser";
         OPTIONS_ADD_TO_MY_LIST_BUTTON = "xpath://XCUIElementTypeButton[@name='Save for later']";
-        ARTICLE_TO_DELETE = "xpath://XCUIElementTypeLink[@name='JavaScript High-level programming language']";
+        ARTICLE_TO_DELETE = "id:Java (programming language) Object-oriented programming language";
         ARTICLE_WITH_TITLE_TO_STAY = "id:JavaScript High-level programming language";
     }
 
